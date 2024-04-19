23ee09fe14cbaa5e511f73fa24ae843edc6a5ab3
Ronggo Hernanda <ronggohernanda@gmail.com>
2024-04-19 21:52:56 +0700

    exercise testing

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func Multiplication(a int, b int) int {
 	return a * b
 }
@@ -10,4 +12,10 @@ func Division(c int, d int) int {
 
 func Modulus(x int, y int) int {
 	return x % y
+}
+
+func main() {
+	fmt.Println(Multiplication(5, 5))
+	fmt.Println(Division(100, 5))
+	fmt.Println(Modulus(10, 3))
 }
